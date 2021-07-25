78cab096fb0af81684867250b981aa45c87660c6
Joseph Wyman <jwymandev@gmail.com>
2021-07-25 11:17:44 -0400

    Initial Commit

---
+++ Universidad De Bastos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Universidad De Bastos
+//
+//  Created by Joseph Wyman on 7/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Universidad De Bastos/Universidad_De_BastosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Universidad_De_BastosApp.swift
+//  Universidad De Bastos
+//
+//  Created by Joseph Wyman on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Universidad_De_BastosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
